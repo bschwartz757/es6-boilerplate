80e9261dfb163f1fa5a0ed92006ee19a06bc05d6
Radek Adamczyk <radekadamczyk@Radoslaws-MacBook-Pro.local>
2015-12-07 11:06:43 -0800

    Adding some doc/comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,21 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         browserify: {
+            /**
+             * Browserify http://browserify.org/ handles commonjs
+             * modules resolves imports, exports, dependencies and
+             * generates a single output files from a set of modules
+             */
             dist: {
                 options: {
                     transform: [
+                        /**
+                         * Using babelify https://github.com/babel/babelify
+                         * as transform to handle EcmaScript6 syntax. Babelify is
+                         * a browserify wrapper for Babel https://babeljs.io/ javascript
+                         * compiler (transpiler) which handles different js syntaxes
+                         * i.e ES6, TypeScript, JSX etc..
+                         */
                         ["babelify", {
                             presets: ["es2015"]
                         }]
@@ -15,12 +27,21 @@ module.exports = function (grunt) {
             }
         },
         watch: {
+            /**
+             * Simple watcher, run 'grunt watch' from commandline
+             * every time you change the files in src folder
+             * browserify will regenerate output files.
+             *
+             */
             scripts: {
                 files: ["./src/**/*.es6"],
                 tasks: ["browserify"]
             }
         },
         serve: {
+            /**
+             * Serving root directory at http://localhost:8080
+             */
             options: {
                 port: 8080
             }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # es6-boilerplate
 Simple boiler plate for es6 learning. 
+
+## install and run
+
+1. install nodejs https://nodejs.org/en/
+2. install grunt-cli, from command line `npm install -g grunt-cli`
+3. update dependencies, from command line in project folder `npm install`
+4. generate combined js file, from command line `grunt build`
+5. run server `grunt serve`
+6. open `http://localhost:8080/index.html`
